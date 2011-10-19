23d97b7a85275d7047f0aa5cfa39bb862ea1eb59
diego.sancheze@pucp.pe <diego.sancheze@pucp.pe@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-19 23:59:14 +0000

    Uk

--- trunk/src/DataAccess/FamiliaDA.java
+++ trunk/src/DataAccess/FamiliaDA.java
@@ -17,41 +17,37 @@ import java.util.ArrayList;
  */
 public class FamiliaDA {
     
-    public String insertar(FamiliaBE objFamilia) throws Exception{
-        conexion cnn = new conexion();
-        cnn.estableceCnn();
-        PreparedStatement pstmt = null;
+    public boolean insertar(FamiliaBE objFamilia) throws Exception{
+        
+        boolean boolExito = false;
+        conexion objConexion = new conexion();
        
         String sql = "INSERT INTO Familia(idfamilia,nombre, descripcion, indactivo) VALUES('"+ objFamilia.getCodigo() +"','"+ objFamilia.getNombre() +"','"+ objFamilia.getDescripcion() +"','"+ objFamilia.getEstado() +"');";
-        pstmt = cnn.cnn.prepareStatement(sql);
+        
         try{
-            pstmt.executeUpdate(); 
-         }catch (Exception a) {	
-             System.out.println(a.getMessage());
-		} finally{
-			//6. Se cierran todos los objetos de conexion. ES EL MAS IMPORTANTE
-			if (pstmt !=null) {try{pstmt.close();}catch(Exception a){System.out.println(a.getMessage());}}
-			if (cnn.cnn !=null) {try{cnn.cnn.close();}catch(Exception a){System.out.println(a.getMessage());}}
-		}
+            objConexion.EjecutarUID(sql);
+            boolExito=true;
+         }catch (Exception a){
+            System.out.println(a.getMessage());
+        }
+        finally{objConexion.SalirUID();}
         
-        
-        return null;
+        return boolExito;
     }
     
     public ArrayList<FamiliaBE> queryAllFamilia(){
-        conexion cnn = new conexion();
-        cnn.estableceCnn();
-        PreparedStatement pstmt = null;
+        conexion objConexion=new conexion();
         ResultSet rs = null;
         ArrayList<FamiliaBE> arrFamilia = new ArrayList<FamiliaBE>();
+        String sql = "SELECT * FROM Familia WHERE IndActivo = 1";
         try{
-             pstmt = cnn.cnn.prepareStatement("SELECT * FROM Familia WHERE IndActivo = 1");
-             String strCodigo;
-             String strNombre;
-             String strDescripcion;
-             String strEstado;
-             rs = pstmt.executeQuery();
-             while (rs.next()) {
+            rs=objConexion.EjecutarS(sql);
+            String strCodigo;
+            String strNombre;
+            String strDescripcion;
+            String strEstado;
+            while (rs.next()){
+              
                 strCodigo = rs.getString(1);
                 strNombre = rs.getString(2);
                 strDescripcion = rs.getString(3);
@@ -59,16 +55,13 @@ public class FamiliaDA {
                 arrFamilia.add(new FamiliaBE(strCodigo,strNombre,strDescripcion,strEstado));
             }
              
-        }catch (Exception a) {			
-			System.out.println(a.getMessage());
-		} finally{
-			
-			if (pstmt !=null) {try{pstmt.close();}catch(Exception a){System.out.println(a.getMessage());}}
-			if (cnn.cnn !=null) {try{cnn.cnn.close();}catch(Exception a){System.out.println(a.getMessage());}}
-			if (rs !=null) {try{rs.close();}catch(Exception a){System.out.println(a.getMessage());}}
-		}
+        }catch (Exception a){
+            System.out.println(a.getMessage());
+         }
+         finally{
+             objConexion.SalirS();
+         }
       
-        
         return arrFamilia;
     }
 }
