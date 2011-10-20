af04777ca1b85015a5014ba4f82c4da3527f33f6
victortsuchida <victortsuchida@gmail.com>
2011-10-20 02:08:43 +0000

    vctr

--- src/BusinessEntity/ProveedorBE.java
+++ src/BusinessEntity/ProveedorBE.java
@@ -15,6 +15,94 @@ public class ProveedorBE {
     private Integer ruc;
     private String nombre;
     private String direccion;
+
+    public Integer getDNIContacto() {
+        return DNIContacto;
+    }
+
+    public void setDNIContacto(Integer DNIContacto) {
+        this.DNIContacto = DNIContacto;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreContacto() {
+        return nombreContacto;
+    }
+
+    public void setNombreContacto(String nombreContacto) {
+        this.nombreContacto = nombreContacto;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+    public Integer getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(Integer ruc) {
+        this.ruc = ruc;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getTelefonoContacto() {
+        return telefonoContacto;
+    }
+
+    public void setTelefonoContacto(Integer telefonoContacto) {
+        this.telefonoContacto = telefonoContacto;
+    }
     private Integer telefono;
     private String paginaWeb;
     private String email;
