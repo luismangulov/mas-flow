0d1ca66bf6a5870447b9f8fa02381eeef3535a92
diego.sancheze <diego.sancheze@pucp.pe>
2011-10-20 07:07:56 +0000

    Uk

--- src/BusinessEntity/GuiaRemisionBE.java
+++ src/BusinessEntity/GuiaRemisionBE.java
@@ -14,4 +14,46 @@ public class GuiaRemisionBE {
     private String codigo;
     private Date fecha;
     private EntidadBE cliente;
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the cliente
+     */
+    public EntidadBE getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(EntidadBE cliente) {
+        this.cliente = cliente;
+    }
 }
