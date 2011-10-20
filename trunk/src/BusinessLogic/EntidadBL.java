6177aed8431bcc0f69451758e44ffe90970ca20c
gsanchezl@pucp.pe <gsanchezl@pucp.pe@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-20 08:22:16 +0000

    f

--- trunk/src/BusinessLogic/EntidadBL.java
+++ trunk/src/BusinessLogic/EntidadBL.java
@@ -5,10 +5,25 @@
 
 package BusinessLogic;
 
+import BD.Utilitario;
+import BusinessEntity.EntidadBE;
+import DataAccess.EntidadDA;
+
 /**
  *
  * @author giuliana
  */
 public class EntidadBL {
-
+        public boolean insertarEntidad( String nroDocumento, String direccion,
+            String telefono, String email, String nombreContacto, String dniContacto,
+            String telefonoContacto, String idTipoPersona, String razonSocial,
+            String paginaWeb, String idTipoEntidad, String indActivo, String idCIUU) throws Exception{
+        boolean exito = false;
+        EntidadBE Entidad = new EntidadBE(Utilitario.generaCodigo("Entidad",6),nroDocumento,
+                direccion,telefono, email, nombreContacto, dniContacto, telefonoContacto,
+                idTipoPersona, razonSocial, paginaWeb, idTipoEntidad, indActivo, idCIUU);
+        EntidadDA objEntidadDA = new EntidadDA();
+        exito = objEntidadDA.insertar(Entidad);
+        return exito;
+    }
 }
