b9d3e6b73b3d077fd109bec5ebb7252819687b53
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-20 02:10:19 +0000

    vctr

--- trunk/src/BusinessEntity/ProductoBE.java
+++ trunk/src/BusinessEntity/ProductoBE.java
@@ -17,4 +17,69 @@ public class ProductoBE {
     private UnidadMedidaBE unidadMedida;
     private double precio;
     private String estado;
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public FamiliaBE getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(FamiliaBE familia) {
+        this.familia = familia;
+    }
+
+    public int getMaxCantPorPallet() {
+        return maxCantPorPallet;
+    }
+
+    public void setMaxCantPorPallet(int maxCantPorPallet) {
+        this.maxCantPorPallet = maxCantPorPallet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public UnidadMedidaBE getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public void setUnidadMedida(UnidadMedidaBE unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
 }
