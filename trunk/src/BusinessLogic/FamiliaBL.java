d439dce36cd43beb66639114f30a86aa51dc3cab
diego.sancheze@pucp.pe <diego.sancheze@pucp.pe@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-20 16:55:54 +0000

    Uk

--- trunk/src/BusinessLogic/FamiliaBL.java
+++ trunk/src/BusinessLogic/FamiliaBL.java
@@ -7,17 +7,32 @@ package BusinessLogic;
 import BD.Utilitario;
 import BusinessEntity.FamiliaBE;
 import DataAccess.FamiliaDA;
+import java.util.ArrayList;
 
 /**
  *
  * @author DIEGO
  */
 public class FamiliaBL {
+    private FamiliaBE familia;
     public boolean insertar(String nombre,String descripcion) throws Exception{
         boolean exito = false;
-        FamiliaBE familia = new FamiliaBE(Utilitario.generaCodigo("familia",6),nombre,descripcion,"1");
+        familia = new FamiliaBE(Utilitario.generaCodigo("familia",6),nombre,descripcion,"1");
         FamiliaDA objFamiliaDA = new FamiliaDA();
-        exito = objFamiliaDA.insertar(familia);
+        exito = objFamiliaDA.insertar(getFamilia());
         return exito;
     }
+    
+    public ArrayList<FamiliaBE> queryAllFamilia(){
+        FamiliaDA objFamiliaDA = new FamiliaDA();
+        return objFamiliaDA.queryAllFamilia();
+    }
+    
+    /**
+     * @return the familia
+     */
+    public FamiliaBE getFamilia() {
+        return familia;
+    }
+    
 }
