1bf8f48a2fd62efd8cd298a782bee80448208bb8
victortsuchida <victortsuchida@gmail.com>
2011-10-20 05:10:05 +0000

    vctr

---
+++ src/BusinessLogic/ProveedorBL.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BusinessLogic;
+import BusinessEntity.ProveedorBE;
+import DataAccess.ProveedorDA;
+/**
+ *
+ * @author victor
+ */
+public class ProveedorBL {
+    public void insertarProveedor(ProveedorBE proveedor){
+
+    }
+}
