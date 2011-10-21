b673902657b84685189e633286b02a477f06c56b
victortsuchida <victortsuchida@gmail.com>
2011-10-21 08:39:52 +0000

    vctr

--- src/BusinessEntity/UnidadMedidaBE.java
+++ src/BusinessEntity/UnidadMedidaBE.java
@@ -9,7 +9,49 @@ package BusinessEntity;
  * @author DIEGO
  */
 public class UnidadMedidaBE {
-    private String codigo;
+    private String idUnidadMedida;
+    private String nombre;
     private String descripcion;
     private String estado;
+
+    public UnidadMedidaBE(){}
+
+    public UnidadMedidaBE(String idUnidadMedida, String nombre, String descripcion, String estado) {
+        this.idUnidadMedida = idUnidadMedida;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getidUnidadMedida() {
+        return idUnidadMedida;
+    }
+
+    public void setidUnidadMedida(String idUnidadMedida) {
+        this.idUnidadMedida = idUnidadMedida;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
