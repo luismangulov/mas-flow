85b764cd7c28eadfdbbc5f8861253b7f782c3922
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-21 21:38:00 +0000

    vctr

--- trunk/src/BusinessLogic/ProductoBL.java
+++ trunk/src/BusinessLogic/ProductoBL.java
@@ -7,14 +7,44 @@ package BusinessLogic;
 
 import BusinessEntity.ProductoBE;
 import DataAccess.ProductoDA;
+import java.util.ArrayList;
 
 /**
  *
  * @author victor
  */
 public class ProductoBL {
+
+    private ProductoDA objProductoDA;
+
     public boolean insertar(ProductoBE producto){
-        ProductoDA productoDA = new ProductoDA();
-        return productoDA.insertar(producto);
+        objProductoDA = new ProductoDA();
+        return objProductoDA.insertar(producto);
     }
+
+    public boolean modificar(ProductoBE productoBE) {
+        objProductoDA = new ProductoDA();
+        return objProductoDA.modificar(productoBE);
+    }
+    
+    public boolean eliminar(ProductoBE productoBE) {
+        objProductoDA = new ProductoDA();
+        return objProductoDA.eliminar(productoBE);
+    }
+    
+    public ProductoBE getByIdProducto(String idProducto) {
+        objProductoDA = new ProductoDA();
+        return objProductoDA.queryByIdProducto(idProducto);
+    }
+
+    public ArrayList<ProductoBE> getAllProductoActivo(){
+        objProductoDA = new ProductoDA();
+        return objProductoDA.queryAllProductoActivo();
+    }
+
+    public ArrayList<ProductoBE> getListSearch(String idProducto, String nombre, String familia){
+        objProductoDA = new ProductoDA();
+        return objProductoDA.queryListSearch(idProducto, nombre, familia);
+    }
+
 }
