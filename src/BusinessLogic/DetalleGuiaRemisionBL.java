240680e8eb25e3c05ecdd329ce2cbe47d4b98963
diego.sancheze <diego.sancheze@pucp.pe>
2011-10-22 00:53:31 +0000

    Uk

---
+++ src/BusinessLogic/DetalleGuiaRemisionBL.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+import BD.Utilitario;
+import BusinessEntity.DetalleGuiaRemisionBE;
+import DataAccess.DetalleGuiaRemisionDA;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class DetalleGuiaRemisionBL {
+    public boolean insertar(String idGuiaRemision,String idProducto,int cantidad,double subtotal) throws FileNotFoundException, IOException, Exception{
+        boolean exito = false;
+        DetalleGuiaRemisionBE objDetalleGuiaRemisionBE;
+        objDetalleGuiaRemisionBE = new DetalleGuiaRemisionBE(Utilitario.generaCodigo("detalleguiaremision",6),cantidad,subtotal,idProducto);
+        DetalleGuiaRemisionDA objDetalleGuiaRemisionDA = new DetalleGuiaRemisionDA();
+        exito = objDetalleGuiaRemisionDA.insertar(objDetalleGuiaRemisionBE,idGuiaRemision);
+        return exito;
+    }
+}
