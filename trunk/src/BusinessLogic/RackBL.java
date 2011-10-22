6ecc2b12817b0d8409328d2d61ffccccca6e69cd
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-22 09:21:45 +0000

    vctr

---
+++ trunk/src/BusinessLogic/RackBL.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+import BusinessEntity.RackBE;
+import DataAccess.RackDA;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author victor
+ */
+public class RackBL {
+    
+    RackDA objRackDA;
+    public void insertar(RackBE objRack){
+        objRackDA = new RackDA();
+        objRackDA.insertar(objRack);
+    }
+    
+    public void modificar(RackBE objRack){
+        objRackDA = new RackDA();
+        objRackDA.modificar(objRack);
+    }
+        
+    public void eliminar(String idRack){
+        objRackDA = new RackDA();
+        objRackDA.eliminar(idRack);
+    }
+     
+    public RackBE getByIdRack(String idRack){
+        objRackDA = new RackDA();
+        return objRackDA.queryByIdRack(idRack);
+    }   
+    
+    public ArrayList<RackBE> getAllRack(){
+        objRackDA = new RackDA();
+        return objRackDA.queryAllActivo();
+    }    
+    
+    public ArrayList<RackBE> getListSearch(String idRack, String idZona){
+        objRackDA = new RackDA();
+        return objRackDA.queryListSearch(idRack, idZona);
+    }
+}
