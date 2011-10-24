9880cb545fc9311dbbb117af85023a7136e2680d
victortsuchida <victortsuchida@gmail.com>
2011-10-24 22:55:59 +0000

    vctr

--- src/BusinessEntity/PalletBE.java
+++ src/BusinessEntity/PalletBE.java
@@ -4,10 +4,78 @@
  */
 package BusinessEntity;
 
+import java.sql.Date;
+
 /**
  *
  * @author victor
  */
 public class PalletBE {
     
+    private String idPallet;
+    private String idProducto;
+    private String indActivo;
+    private String idUbicacion;
+    private String idAlmacen;
+    private Date fechaVencimiento;
+
+    public PalletBE() {
+    }
+
+    public PalletBE(String idPallet, String idProducto, String indActivo, String idUbicacion, String idAlmacen, Date fechaVencimiento) {
+        this.idPallet = idPallet;
+        this.idProducto = idProducto;
+        this.indActivo = indActivo;
+        this.idUbicacion = idUbicacion;
+        this.idAlmacen = idAlmacen;
+        this.fechaVencimiento = fechaVencimiento;
+    }
+    
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getIdAlmacen() {
+        return idAlmacen;
+    }
+
+    public void setIdAlmacen(String idAlmacen) {
+        this.idAlmacen = idAlmacen;
+    }
+
+    public String getIdPallet() {
+        return idPallet;
+    }
+
+    public void setIdPallet(String idPallet) {
+        this.idPallet = idPallet;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getIdUbicacion() {
+        return idUbicacion;
+    }
+
+    public void setIdUbicacion(String idUbicacion) {
+        this.idUbicacion = idUbicacion;
+    }
+
+    public String getIndActivo() {
+        return indActivo;
+    }
+
+    public void setIndActivo(String indActivo) {
+        this.indActivo = indActivo;
+    }   
 }
