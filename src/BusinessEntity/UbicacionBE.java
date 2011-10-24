542ce3b1111ac97ebaad5afd04410db60e193c72
victortsuchida <victortsuchida@gmail.com>
2011-10-24 05:49:56 +0000

    vctr

---
+++ src/BusinessEntity/UbicacionBE.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessEntity;
+
+/**
+ *
+ * @author victor
+ */
+public class UbicacionBE {
+    private String idUbicacion;
+    private int fila;
+    private int columna;
+    private String indActivo;
+    private String idRack;
+
+    public UbicacionBE() {
+    }
+
+    public UbicacionBE(String idUbicacion, int fila, int columna, String indActivo, String idRack) {
+        this.idUbicacion = idUbicacion;
+        this.fila = fila;
+        this.columna = columna;
+        this.indActivo = indActivo;
+        this.idRack = idRack;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public String getIdRack() {
+        return idRack;
+    }
+
+    public void setIdRack(String idRack) {
+        this.idRack = idRack;
+    }
+
+    public String getIdUbicacion() {
+        return idUbicacion;
+    }
+
+    public void setIdUbicacion(String idUbicacion) {
+        this.idUbicacion = idUbicacion;
+    }
+
+    public String getIndActivo() {
+        return indActivo;
+    }
+
+    public void setIndActivo(String indActivo) {
+        this.indActivo = indActivo;
+    }
+}
