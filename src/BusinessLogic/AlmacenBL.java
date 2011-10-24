3a37ca03e3642c65c4fdcededff329e99cb66d64
victortsuchida <victortsuchida@gmail.com>
2011-10-24 22:43:53 +0000

    vctr

---
+++ src/BusinessLogic/AlmacenBL.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+import BusinessEntity.AlmacenBE;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author victor
+ */
+public class AlmacenBL {
+    
+    ArrayList<AlmacenBE> arrAlmacenes;
+    
+    public void insertar(AlmacenBE objAlmacenBE){
+        
+    }
+    
+    public void modificar(AlmacenBE objAlmacenBE){
+        
+    }
+    
+    public void eliminar(String idAlmacen){
+        
+    }
+    
+    public ArrayList<AlmacenBE> getAllAlmacenActivo(){
+        
+        arrAlmacenes = new ArrayList<AlmacenBE>();
+        return arrAlmacenes;
+        
+    }
+    
+    
+}
