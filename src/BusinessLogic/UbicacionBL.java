7455d7667b315284dd8a504e6bff0a620c300abc
victortsuchida <victortsuchida@gmail.com>
2011-10-24 22:56:16 +0000

    vctr

---
+++ src/BusinessLogic/UbicacionBL.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+import BusinessEntity.UbicacionBE;
+import DataAccess.UbicacionDA;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author victor
+ */
+public class UbicacionBL {
+    
+    UbicacionDA objUbicacionDA;
+    
+    public void insertar(UbicacionBE objUbicacionBE){
+        objUbicacionDA = new UbicacionDA();
+        objUbicacionDA.insertar(objUbicacionBE);
+    }
+    
+    public void bloquearUbicacion(String idUbicacion){
+        
+    }
+
+    public ArrayList<UbicacionBE> getUbicacionesByRack(String idRack) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+            
+}
