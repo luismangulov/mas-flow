3903dd363b9f6cc6c69abb71e1f2ea1d51caccbb
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-25 01:55:03 +0000

    vctr

--- trunk/src/DataAccess/ZonaDA.java
+++ trunk/src/DataAccess/ZonaDA.java
@@ -27,7 +27,7 @@ public class ZonaDA {
         ResultSet rs = null;
         arrZonas = new ArrayList<ZonaBE>();
         
-        query = "SELECT idZona,nombre,descripcion,indActivo,idAlmacen FROM Zona WHERE IndActivo = '1' order by 1";
+        query = "SELECT * FROM Zona WHERE IndActivo = '1' order by 1";
         
         try{
             rs = objConexion.EjecutarS(query);
@@ -36,16 +36,25 @@ public class ZonaDA {
             String strDescripcion;
             String strIndActivo;
             String strIdAlmacen;
+            Double douLargo;
+            Double douAncho;
+            int intPosX;
+            int intPosY;
+            String strIdentificador;
             
             while (rs.next()){
               
                 strIdZona = rs.getString("idZona");
                 strNombre = rs.getString("nombre");
-                strDescripcion = rs.getString("descripcion");
                 strIndActivo = rs.getString("indActivo");
                 strIdAlmacen = rs.getString("idAlmacen");
+                douLargo = rs.getDouble("Largo");
+                douAncho = rs.getDouble("Ancho");
+                intPosX = rs.getInt("PosX");
+                intPosY = rs.getInt("posY");
+                strIdentificador = rs.getString("Identificador");
                 
-                arrZonas.add(new ZonaBE(strIdZona,strNombre,strDescripcion,strIndActivo,strIdAlmacen));
+                arrZonas.add(new ZonaBE(strIdZona,strNombre,strIndActivo,strIdAlmacen,douLargo,douAncho,intPosX,intPosY,strIdentificador));
             }
              
         }catch (Exception e){
@@ -63,7 +72,7 @@ public class ZonaDA {
         objConexion = new conexion();
         ResultSet rs = null;
         
-        query = "SELECT idZona,nombre,descripcion,indActivo,idAlmacen FROM Zona WHERE idZona = '"+idZona+"'";
+        query = "SELECT * FROM Zona WHERE idZona = '"+idZona+"'";
         
         try{
             rs = objConexion.EjecutarS(query);
@@ -72,15 +81,24 @@ public class ZonaDA {
             String strDescripcion;
             String strIndActivo;
             String strIdAlmacen;
+            Double douLargo;
+            Double douAncho;
+            int intPosX;
+            int intPosY;
+            String strIdentificador;
             
             rs.next();
             strIdZona = rs.getString("idZona");
             strNombre = rs.getString("nombre");
-            strDescripcion = rs.getString("descripcion");
             strIndActivo = rs.getString("indActivo");
             strIdAlmacen = rs.getString("idAlmacen");
-
-            objZonaBE = new ZonaBE(strIdZona,strNombre,strDescripcion,strIndActivo,strIdAlmacen);
+            douLargo = rs.getDouble("Largo");
+            douAncho = rs.getDouble("Ancho");
+            intPosX = rs.getInt("PosX");
+            intPosY = rs.getInt("posY");
+            strIdentificador = rs.getString("Identificador");
+            
+            objZonaBE = new ZonaBE(strIdZona,strNombre,strIndActivo,strIdAlmacen,douLargo,douAncho,intPosX,intPosY,strIdentificador);
             
         }catch (Exception e){
             JOptionPane.showMessageDialog(null, e, query, 0);
@@ -92,12 +110,12 @@ public class ZonaDA {
       return objZonaBE;
     }
     
-    public ZonaBE queryByNombreZona(String nombreZona){
+    public ZonaBE queryByIdentificadorZona(String identificador){
         
         objConexion = new conexion();
         ResultSet rs = null;
         
-        query = "SELECT idZona,nombre,descripcion,indActivo,idAlmacen FROM Zona WHERE nombre = '"+nombreZona+"'";
+        query = "SELECT * FROM Zona WHERE identificador = '"+identificador+"'";
         
         try{
             rs = objConexion.EjecutarS(query);
@@ -106,15 +124,24 @@ public class ZonaDA {
             String strDescripcion;
             String strIndActivo;
             String strIdAlmacen;
+            Double douLargo;
+            Double douAncho;
+            int intPosX;
+            int intPosY;
+            String strIdentificador;
             
             rs.next();
             strIdZona = rs.getString("idZona");
             strNombre = rs.getString("nombre");
-            strDescripcion = rs.getString("descripcion");
             strIndActivo = rs.getString("indActivo");
             strIdAlmacen = rs.getString("idAlmacen");
-
-            objZonaBE = new ZonaBE(strIdZona,strNombre,strDescripcion,strIndActivo,strIdAlmacen);
+            douLargo = rs.getDouble("Largo");
+            douAncho = rs.getDouble("Ancho");
+            intPosX = rs.getInt("PosX");
+            intPosY = rs.getInt("posY");
+            strIdentificador = rs.getString("Identificador");
+            
+            objZonaBE = new ZonaBE(strIdZona,strNombre,strIndActivo,strIdAlmacen,douLargo,douAncho,intPosX,intPosY,strIdentificador);
             
         }catch (Exception e){
             JOptionPane.showMessageDialog(null, e, query, 0);
