074b0421ac4f713e6f23a0666a90ed58c0c16858
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-25 08:31:14 +0000

    vctr

--- trunk/src/BusinessLogic/PalletBL.java
+++ trunk/src/BusinessLogic/PalletBL.java
@@ -4,10 +4,38 @@
  */
 package BusinessLogic;
 
+import BusinessEntity.PalletBE;
+import DataAccess.PalletDA;
+import java.util.ArrayList;
+
 /**
  *
  * @author victor
  */
 public class PalletBL {
+
+    PalletDA objPalletDA;
+    ArrayList<PalletBE> arrPallets;
+    
+    public void insertar(PalletBE objPalletBE) {
+        objPalletDA = new PalletDA();
+        objPalletDA.insertar(objPalletBE);
+    }
+    
+//    public void modificar(PalletBE objPalletBE) {
+//        objPalletDA = new PalletDA();
+//        objPalletDA.modificar(objPalletBE);
+//    }
+        
+    public void eliminar(String strIdPallet) {
+        objPalletDA = new PalletDA();
+        objPalletDA.eliminar(strIdPallet);
+    }
+
+    public ArrayList<PalletBE> getListSearch(String idPallet, String idProducto, String idAlmacen) {
+        objPalletDA = new PalletDA();
+        return objPalletDA.queryListSearch(idPallet, idProducto, idAlmacen);
+    }
+    
     
 }
