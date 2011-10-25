ff3299005729bafa80498fa0a5dce3a41b6a7a1a
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-25 08:32:13 +0000

    vctr

--- trunk/src/Mantenimientos/Ubicacion/AdmUbicacion.java
+++ trunk/src/Mantenimientos/Ubicacion/AdmUbicacion.java
@@ -33,9 +33,9 @@ public class AdmUbicacion extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jToolBar1 = new javax.swing.JToolBar();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        lblBloquearUbicacion = new javax.swing.JLabel();
+        lblBuscar = new javax.swing.JLabel();
+        lblRefrescar = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
 
@@ -77,19 +77,27 @@ public class AdmUbicacion extends javax.swing.JFrame {
 
         jToolBar1.setRollover(true);
 
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/lock.png"))); // NOI18N
-        jToolBar1.add(jLabel8);
-
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/search_page.png"))); // NOI18N
-        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                jLabel4MousePressed(evt);
+        lblBloquearUbicacion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/lock.png"))); // NOI18N
+        lblBloquearUbicacion.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblBloquearUbicacionMouseClicked(evt);
             }
         });
-        jToolBar1.add(jLabel4);
+        jToolBar1.add(lblBloquearUbicacion);
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/refresh.png"))); // NOI18N
-        jToolBar1.add(jLabel5);
+        lblBuscar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/search_page.png"))); // NOI18N
+        lblBuscar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblBuscarMouseClicked(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                lblBuscarMousePressed(evt);
+            }
+        });
+        jToolBar1.add(lblBuscar);
+
+        lblRefrescar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/refresh.png"))); // NOI18N
+        jToolBar1.add(lblRefrescar);
 
         jLabel7.setText("                                                                                                                                                               ");
         jToolBar1.add(jLabel7);
@@ -106,9 +114,8 @@ public class AdmUbicacion extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-            .addGap(0, 400, Short.MAX_VALUE)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 404, Short.MAX_VALUE)
+            .addGap(0, 404, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -118,10 +125,19 @@ public class AdmUbicacion extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-private void jLabel4MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MousePressed
-BuscarUbicacion m = new BuscarUbicacion();
-m.setVisible(true);// TODO add your handling code here:
-}//GEN-LAST:event_jLabel4MousePressed
+private void lblBuscarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBuscarMousePressed
+
+}//GEN-LAST:event_lblBuscarMousePressed
+
+private void lblBloquearUbicacionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBloquearUbicacionMouseClicked
+    BloquearUbicacion ventana = new BloquearUbicacion();
+    ventana.setVisible(true);
+}//GEN-LAST:event_lblBloquearUbicacionMouseClicked
+
+private void lblBuscarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblBuscarMouseClicked
+    BuscarUbicacion ventana = new BuscarUbicacion();
+    ventana.setVisible(true);
+}//GEN-LAST:event_lblBuscarMouseClicked
 
     /**
      * @param args the command line arguments
@@ -159,13 +175,13 @@ m.setVisible(true);// TODO add your handling code here:
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JLabel lblBloquearUbicacion;
+    private javax.swing.JLabel lblBuscar;
+    private javax.swing.JLabel lblRefrescar;
     // End of variables declaration//GEN-END:variables
 }
