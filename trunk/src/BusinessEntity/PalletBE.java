f158fe6ae3379983478f23a073e370db3c22bf1a
victortsuchida@gmail.com <victortsuchida@gmail.com@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-10-25 06:38:49 +0000

    vctr

--- trunk/src/BusinessEntity/PalletBE.java
+++ trunk/src/BusinessEntity/PalletBE.java
@@ -14,7 +14,7 @@ public class PalletBE {
     
     private String idPallet;
     private String idProducto;
-    private String indActivo;
+    private String indActivo; // 0: vacío, 1: ocupado, 2: averiado
     private String idUbicacion;
     private String idAlmacen;
     private Date fechaVencimiento;
