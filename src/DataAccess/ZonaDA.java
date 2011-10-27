de2c5150b64309b6323dea8a9835f4426a26a1e2
gsanchezl <gsanchezl@pucp.pe>
2011-10-27 14:39:55 +0000

    f

--- src/DataAccess/ZonaDA.java
+++ src/DataAccess/ZonaDA.java
@@ -16,22 +16,22 @@ import BusinessEntity.FamiliaBE;
  * @author victor
  */
 public class ZonaDA {
-    
+
     ArrayList<ZonaBE> arrZonas;
     ZonaBE objZonaBE;
     conexion objConexion;
     String query;
-    
-    
+
+
     public ArrayList<ZonaBE> queryAllZona(){
-        
+
         objConexion = new conexion();
         ResultSet rs = null;
         arrZonas = new ArrayList<ZonaBE>();
-        
+
         query = "SELECT idZona, nombre, identificador, indActivo,"
                 + " idAlmacen, posX, posY, ancho, largo FROM Zona order by 1";
-        
+
         try{
             rs = objConexion.EjecutarS(query);
             String strIdZona;
@@ -44,9 +44,9 @@ public class ZonaDA {
             int intAncho;
             int intLargo;
             ArrayList <FamiliaBE> familias;
-            
+
             while (rs.next()){
-              
+
                 strIdZona = rs.getString(1);
                 strNombre = rs.getString(2);
                 strIdentificador = rs.getString(3);
@@ -56,32 +56,32 @@ public class ZonaDA {
                 intY= rs.getInt(7);
                 intAncho= rs.getInt(8);
                 intLargo= rs.getInt(9);
-                
+
                 familias= querryAllFamilias(strIdZona);
-                
+
                 arrZonas.add(new ZonaBE(strIdZona, strNombre, strIdentificador, strIndActivo,
                 strIdAlmacen, intX, intY, intAncho, intLargo, familias));
             }
-             
+
         }catch (Exception e){
             System.out.println(e.getMessage());
          }
          finally{
              objConexion.SalirS();
          }
-      
+
         return arrZonas;
     }
-    
+
     public ArrayList<ZonaBE> queryAllZonaActiva(){
-        
+
         objConexion = new conexion();
         ResultSet rs = null;
         arrZonas = new ArrayList<ZonaBE>();
-        
+
         query = "SELECT idZona, nombre, identificador, indActivo,"
                 + " idAlmacen, posX, posY, ancho, largo FROM Zona WHERE IndActivo = '1' order by 1";
-        
+
         try{
             rs = objConexion.EjecutarS(query);
             String strIdZona;
@@ -94,9 +94,9 @@ public class ZonaDA {
             int intAncho;
             int intLargo;
             ArrayList <FamiliaBE> familias;
-            
+
             while (rs.next()){
-              
+
                 strIdZona = rs.getString(1);
                 strNombre = rs.getString(2);
                 strIdentificador = rs.getString(3);
@@ -106,34 +106,34 @@ public class ZonaDA {
                 intY= rs.getInt(7);
                 intAncho= rs.getInt(8);
                 intLargo= rs.getInt(9);
-                
+
                 familias= querryAllFamilias(strIdZona);
-                
+
                 arrZonas.add(new ZonaBE(strIdZona, strNombre, strIdentificador, strIndActivo,
                 strIdAlmacen, intX, intY, intAncho, intLargo, familias));
             }
-             
+
         }catch (Exception e){
             System.out.println(e.getMessage());
          }
          finally{
              objConexion.SalirS();
          }
-      
+
         return arrZonas;
     }
-    
-    
-    
+
+
+
     public ZonaBE queryByIdZona(String idZona){
-        
+
         objConexion = new conexion();
         ResultSet rs = null;
         arrZonas = new ArrayList<ZonaBE>();
-        
+
         query = "SELECT idZona, nombre, identificador, indActivo,"
                 + " idAlmacen, posX, posY, ancho, largo FROM Zona WHERE IndActivo = '1' and idZona = '"+idZona+"' order by 1";
-        
+
         try{
             rs = objConexion.EjecutarS(query);
             String strIdZona;
@@ -146,9 +146,9 @@ public class ZonaDA {
             int intAncho;
             int intLargo;
             ArrayList <FamiliaBE> familias;
-            
+
             if (rs.next()){
-              
+
                 strIdZona = rs.getString(1);
                 strNombre = rs.getString(2);
                 strIdentificador = rs.getString(3);
@@ -163,18 +163,18 @@ public class ZonaDA {
                 objZonaBE= new ZonaBE(strIdZona, strNombre, strIdentificador, strIndActivo,
                 strIdAlmacen, intX, intY, intAncho, intLargo, familias);
             }
-            
+
         }catch (Exception e){
             JOptionPane.showMessageDialog(null, e, query, 0);
          }
          finally{
              objConexion.SalirS();
          }
-        
+
       return objZonaBE;
     }
-    
-    
+
+
     private ArrayList<FamiliaBE> querryAllFamilias(String strIdZona) {
         conexion objConexion=new conexion();
         ResultSet rs = null;
@@ -195,14 +195,14 @@ public class ZonaDA {
             strIndActivo = rs.getString(4);
             arrFamilia.add(new FamiliaBE(strIdFamilia,strNombre,strDescripcion,strIndActivo));
             }
-             
+
         }catch (Exception a){
             System.out.println(a.getMessage());
          }
          finally{
              objConexion.SalirS();
          }
-      
+
         return arrFamilia;
     }
 
@@ -216,14 +216,14 @@ public class ZonaDA {
                 + zona.getIdentificador() +"','"+ zona.getIndActivo()
                 +"','"+ zona.getIdAlmacen()
                 +"',"+ zona.getPosX() +","+ zona.getPosY()
-                +","+ zona.getAncho()+","+ zona.getLargo() 
-                +") ";
-        
+                +","+ zona.getAncho()+","+ zona.getLargo()
+                +") ;";
+
         if (!zona.getFamilias().isEmpty()){
             int i=0;
             while (i<zona.getFamilias().size()){
                 sql= sql + "INSERT INTO zonaXfamilia(idZona, idFamilia) values ('"+zona.getIdZona()
-                        +"','"+zona.getFamilias().get(i).getIdFamilia()+"'; ";
+                        +"','"+zona.getFamilias().get(i).getIdFamilia()+"'); ";
                 i++;
             }
         }
@@ -253,16 +253,16 @@ public class ZonaDA {
                     "largo="+objZona.getLargo()+ ","+
                     " WHERE idZona='"+objZona.getIdZona()+"'; "
                     + "DELETE FROM zonaXfamilia WHERE idZona='"+objZona.getIdZona()+"'; ";
-                         
+
             if (!objZona.getFamilias().isEmpty()){
                 int i=0;
                 while (i<objZona.getFamilias().size()){
                 sql= sql + "INSERT INTO zonaXfamilia(idZona, idFamilia) values ('"+objZona.getIdZona()
-                        +"','"+objZona.getFamilias().get(i).getIdFamilia()+"'; ";
+                        +"','"+objZona.getFamilias().get(i).getIdFamilia()+"'); ";
                 i++;
             }
         }
-       
+
         try{
             objConexion.EjecutarUID(sql);
             boolExito=true;
@@ -278,10 +278,10 @@ public class ZonaDA {
     public boolean eliminar(String codZona) {
         boolean boolExito = false;
         objConexion = new conexion();
-       
+
         String sql = "DELETE FROM ZonaXfamilia WHERE idzona='"+codZona+"'; "
                 + "DELETE FROM zona WHERE idZona='"+codZona+"'; ";
-        
+
         try{
             objConexion.EjecutarUID(sql);
             boolExito=true;
@@ -289,7 +289,7 @@ public class ZonaDA {
             System.out.println(a.getMessage());
         }
         finally{objConexion.SalirUID();}
-        
+
         return boolExito;
     }
 
@@ -395,22 +395,22 @@ public class ZonaDA {
         return arrZonas;
 
     }
-    
+
     // lo agregué yo (víctor)
-    
+
         public ArrayList<ZonaBE> queryZonasByAlmacen(String idAlmacen) {
-        
+
         objConexion = new conexion();
         ResultSet rs = null;
         arrZonas = new ArrayList<ZonaBE>();
-        
+
         query = "SELECT * FROM Zona WHERE IndActivo = '1' AND idAlmacen ='" +idAlmacen +"'";
-        
+
         try{
-            rs = objConexion.EjecutarS(query);     
-            
+            rs = objConexion.EjecutarS(query);
+
             while(rs.next()){
-              
+
             String strIdZona = rs.getString("idZona");
             String strNombre = rs.getString("nombre");
             String strIndActivo = rs.getString("indActivo");
@@ -420,30 +420,30 @@ public class ZonaDA {
             int intPosX = rs.getInt("PosX");
             int intPosY = rs.getInt("posY");
             String strIdentificador = rs.getString("Identificador");
-                
+
             arrZonas.add(objZonaBE = new ZonaBE(strIdZona,strNombre,strIdentificador,strIndActivo,strIdAlmacen,intPosX,intPosY,intAncho,intLargo,null));
             }
-            
+
         }catch (Exception e){
             System.out.println(e.getMessage());
          }
          finally{
              objConexion.SalirS();
          }
-      
+
         return arrZonas;
     }
-        
+
     public ZonaBE queryByIdentificadorZona(String identificador){
-        
+
         objConexion = new conexion();
         ResultSet rs = null;
         // aquí cambié el código víctor
         query = "SELECT * FROM Zona WHERE identificador = '"+identificador+"'";
-        
+
         try{
             rs = objConexion.EjecutarS(query);
-            
+
             rs.next();
             String strIdZona = rs.getString("idZona");
             String strNombre = rs.getString("nombre");
@@ -454,16 +454,16 @@ public class ZonaDA {
             int intPosX = rs.getInt("PosX");
             int intPosY = rs.getInt("posY");
             String strIdentificador = rs.getString("Identificador");
-            
+
             objZonaBE = new ZonaBE(strIdZona,strNombre,strIdentificador,strIndActivo,strIdAlmacen,intPosX,intPosY,intAncho,intLargo,null);
-            
+
         }catch (Exception e){
             JOptionPane.showMessageDialog(null, e, query, 0);
          }
          finally{
              objConexion.SalirS();
          }
-        
+
       return objZonaBE;
     }
 }
