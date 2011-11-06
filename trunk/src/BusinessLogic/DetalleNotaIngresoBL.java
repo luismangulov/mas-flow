905745d10aeda1ca7b4bf5eb92288f8fe66e5c57
diego.sancheze@pucp.pe <diego.sancheze@pucp.pe@76aedad7-63e3-5a4d-9ea8-3d370f7dbdbf>
2011-11-06 14:24:50 +0000

    Uk

--- trunk/src/BusinessLogic/DetalleNotaIngresoBL.java
+++ trunk/src/BusinessLogic/DetalleNotaIngresoBL.java
@@ -22,7 +22,7 @@ public class DetalleNotaIngresoBL {
         boolean exito = false;
         DetalleNotaIngresoBE objDetalleNotaIngresoBE;
         
-        //objDetalleNotaIngresoBE = new DetalleNotaIngresoBE(Utilitario.generaCodigoDetalle("detallenotaingreso","idnotaingreso",idNotaIngreso,6),cantidad,idProducto);
+        //objDetalleNotaIngresoBE = new DetalleNotaIngresoBE(Utilitario.generaCodigoDetalle("detallenotaingreso","idnotaingreso",idNotaIngreso,6),cantidad,idProducto,fecha);
         objDetalleNotaIngresoBE = new DetalleNotaIngresoBE(Utilitario.generaCodigo("detallenotaingreso", 6),cantidad,idProducto,fecha);
         DetalleNotaIngresoDA objDetalleNotaIngresoDA = new DetalleNotaIngresoDA();
         exito = objDetalleNotaIngresoDA.insertar(objDetalleNotaIngresoBE,idNotaIngreso);
